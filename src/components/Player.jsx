80ca6e0b0efc2fe22076ab5aa9a3fcf251b0b020
lokofanmoscow-rgb <lokofanmoscow@gmail.com>
2025-09-27 15:16:09 +0300

    Create src/components/Player.jsx

---
+++ src/components/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Player = () => (
+  <div>
+    <button>Play</button>
+    <button>Pause</button>
+    {/* Здесь будет логика воспроизведения */}
+  </div>
+);
+
+export default Player;
